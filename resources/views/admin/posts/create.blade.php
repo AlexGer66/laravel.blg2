d13ea1b61a86a9a63d0eb619dad168dee61f06d6
AlexGer66 <Alexmbi66@gmail.com>
2023-06-06 19:10:39 +0300

    yhf

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -29,31 +29,34 @@
                         </div>
                         <!-- /.card-header -->
 
-                        <form role="form" method="post" action="{{ route('posts.store') }}" enctype="multipart/form-data">
+                        <form role="form" method="post" action="{{ route('posts.store') }}"
+                            enctype="multipart/form-data">
                             @csrf
                             <div class="card-body">
                                 <div class="form-group">
                                     <label for="title">Название</label>
                                     <input type="text" name="title"
-                                           class="form-control @error('title') is-invalid @enderror" id="title"
-                                           placeholder="Название">
+                                        class="form-control @error('title') is-invalid @enderror" id="title"
+                                        placeholder="Название">
                                 </div>
 
                                 <div class="form-group">
                                     <label for="description">Цитата</label>
-                                    <textarea name="description" class="form-control @error('description') is-invalid @enderror" id="description" rows="3" placeholder="Цитата ..."></textarea>
+                                    <textarea name="description" class="form-control @error('description') is-invalid @enderror" id="description"
+                                        rows="3" placeholder="Цитата ..."></textarea>
                                 </div>
 
                                 <div class="form-group">
                                     <label for="content">Контент</label>
                                     <textarea name="content" class="form-control @error('content') is-invalid @enderror" id="content" rows="7"
-                                              placeholder="Контент ..."></textarea>
+                                        placeholder="Контент ..."></textarea>
                                 </div>
 
                                 <div class="form-group">
                                     <label for="category_id">Категория</label>
-                                    <select class="form-control @error('category_id') is-invalid @enderror" id="category_id" name="category_id">
-                                        @foreach($categories as $k => $v)
+                                    <select class="form-control @error('category_id') is-invalid @enderror" id="category_id"
+                                        name="category_id">
+                                        @foreach ($categories as $k => $v)
                                             <option value="{{ $k }}">{{ $v }}</option>
                                         @endforeach
                                     </select>
@@ -62,8 +65,8 @@
                                 <div class="form-group">
                                     <label for="tags">Теги</label>
                                     <select name="tags[]" id="tags" class="select2" multiple="multiple"
-                                            data-placeholder="Выбор тегов" style="width: 100%;">
-                                        @foreach($tags as $k => $v)
+                                        data-placeholder="Выбор тегов" style="width: 100%;">
+                                        @foreach ($tags as $k => $v)
                                             <option value="{{ $k }}">{{ $v }}</option>
                                         @endforeach
                                     </select>
@@ -73,8 +76,7 @@
                                     <label for="thumbnail">Изображение</label>
                                     <div class="input-group">
                                         <div class="custom-file">
-                                            <input type="file" name="thumbnail" id="thumbnail"
-                                                   class="custom-file-input">
+                                            <input type="file" name="thumbnail" id="thumbnail" class="custom-file-input">
                                             <label class="custom-file-label" for="thumbnail">Choose file</label>
                                         </div>
                                     </div>
--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -29,48 +29,47 @@
                         </div>
                         <!-- /.card-header -->
                         <div class="card-body">
+
                             <a href="{{ route('posts.create') }}" class="btn btn-primary mb-3">Добавить статью</a>
                             @if (count($posts))
                                 <div class="table-responsive">
                                     <table class="table table-bordered table-hover text-nowrap">
                                         <thead>
-                                        <tr>
-                                            <th style="width: 30px">#</th>
-                                            <th>Наименование</th>
-                                            <th>Категория</th>
-                                            <th>Теги</th>
-                                            <th>Дата</th>
-                                            <th>Actions</th>
-                                        </tr>
+                                            <tr>
+                                                <th style="width: 30px">#</th>
+                                                <th>Наименование</th>
+                                                <th>Категория</th>
+                                                <th>Теги</th>
+                                                <th>Дата</th>
+                                                <th>Actions</th>
+                                            </tr>
                                         </thead>
                                         <tbody>
-                                        @foreach($posts as $post)
-                                            <tr>
-                                                <td>{{ $post->id }}</td>
-                                                <td>{{ $post->title }}</td>
-                                                <td>{{ $post->category->title }}</td>
-                                                <td>{{ $post->tags->pluck('title')->join(', ') }}</td>
-                                                <td>{{ $post->created_at }}</td>
-                                                <td>
-                                                    <a href="{{ route('posts.edit', ['post' => $post->id]) }}"
-                                                       class="btn btn-info btn-sm float-left mr-1">
-                                                        <i class="fas fa-pencil-alt"></i>
-                                                    </a>
+                                            @foreach ($posts as $post)
+                                                <tr>
+                                                    <td>{{ $post->id }}</td>
+                                                    <td>{{ $post->title }}</td>
+                                                    <td>{{ $post->category->title }}</td>
+                                                    <td>{{ $post->tags->pluck('title')->join(', ') }}</td>
+                                                    <td>{{ $post->created_at }}</td>
+                                                    <td>
+                                                        <a href="{{ route('posts.edit', ['post' => $post->id]) }}"
+                                                            class="btn btn-info btn-sm float-left mr-1">
+                                                            <i class="fas fa-pencil-alt"></i>
+                                                        </a>
 
-                                                    <form
-                                                        action="{{ route('posts.destroy', ['post' => $post->id]) }}"
-                                                        method="post" class="float-left">
-                                                        @csrf
-                                                        @method('DELETE')
-                                                        <button type="submit" class="btn btn-danger btn-sm"
+                                                        <form action="{{ route('posts.destroy', ['post' => $post->id]) }}"
+                                                            method="post" class="float-left">
+                                                            @csrf
+                                                            @method('DELETE')
+                                                            <button type="submit" class="btn btn-danger btn-sm"
                                                                 onclick="return confirm('Подтвердите удаление')">
-                                                            <i
-                                                                class="fas fa-trash-alt"></i>
-                                                        </button>
-                                                    </form>
-                                                </td>
-                                            </tr>
-                                        @endforeach
+                                                                <i class="fas fa-trash-alt"></i>
+                                                            </button>
+                                                        </form>
+                                                    </td>
+                                                </tr>
+                                            @endforeach
                                         </tbody>
                                     </table>
                                 </div>
@@ -93,4 +92,3 @@
     </section>
     <!-- /.content -->
 @endsection
-
